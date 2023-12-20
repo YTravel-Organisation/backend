e41733745174fa382c69a2bf0bb78393f0289d55
Louis.S <louis.sasse@protonmail.com>
2023-12-20 16:20:24 +0100

    fix: Update RabbitMQ URL in smtp.queue.ts

--- apps/smtp/src/main.ts
+++ apps/smtp/src/main.ts
@@ -3,9 +3,10 @@ import { SmtpModule } from './smtp.module';
 import { smtpMicroserviceOptions } from 'lib/queues/smtp.queue';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(SmtpModule, {
+  const app = await NestFactory.createMicroservice(
+    SmtpModule,
     smtpMicroserviceOptions,
-  });
+  );
   await app.listen();
 }
 bootstrap();
--- lib/queues/smtp.queue.ts
+++ lib/queues/smtp.queue.ts
@@ -3,7 +3,7 @@ import { RmqOptions, Transport } from '@nestjs/microservices';
 export const smtpMicroserviceOptions: RmqOptions = {
   transport: Transport.RMQ,
   options: {
-    urls: [`amqp://localhost:5672`],
+    urls: [`amqp://rabbitmq:5672`],
     queue: 'smtp_queue',
   },
 };
