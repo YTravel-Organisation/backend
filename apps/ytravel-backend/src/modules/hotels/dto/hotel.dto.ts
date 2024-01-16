9e5e81cbe62c905c3d00ceeb88efaf2885f5dd13
LSASSE <louis.sasse@protonmail.com>
2024-01-16 11:07:15 +0100

    feat: Add hotel module with controller, service, and DTO

---
+++ apps/ytravel-backend/src/modules/hotels/dto/hotel.dto.ts
@@ -0,0 +1,9 @@
+export class CreateHotelDto {
+    name: string;
+    description: string;
+    address: string;
+    email: string;
+    website: string;
+    phone_number: string;
+    rating: number;
+}
---
+++ apps/ytravel-backend/src/modules/hotels/hotels.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from "@nestjs/common";
+
+Controller("hotels")
+export class HotelsController {
+    constructor() {}
+
+    
+}
