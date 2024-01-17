6672dd7e258795b786a824a8ff058cbc45243504
LSASSE <louis.sasse@protonmail.com>
2024-01-17 12:29:54 +0100

    feat: add swagger in hotel controller

--- apps/ytravel-backend/src/modules/hotels/hotels.controller.ts
+++ apps/ytravel-backend/src/modules/hotels/hotels.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, InternalServerErrorException, Post } from "@nestjs/common";
 import { CreateHotelDto } from "./dto/hotel.dto";
 import { HotelsService } from "./hotels.service";
+import { ApiTags } from "@nestjs/swagger";
 
+ApiTags("hotels")
 Controller("hotels")
 export class HotelsController {
 
     constructor(private readonly hotelsService: HotelsService ) {}
 
+    
     @Post()
     async createHotel(@Body() createHotelDto: CreateHotelDto) {
         try {
