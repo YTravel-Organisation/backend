f0565d7a1a83db4cc68369c7454d5fac41004f93
LSASSE <louis.sasse@protonmail.com>
2024-01-17 09:58:03 +0100

    feat: Add PrismaService to hotels module

--- apps/ytravel-backend/src/modules/hotels/hotels.module.ts
+++ apps/ytravel-backend/src/modules/hotels/hotels.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { HotelsController } from "./hotels.controller";
 import { HotelsService } from "./hotels.service";
+import { PrismaService } from "../../tools/database.config";
 
 Module({
     imports: [],
-    providers: [HotelsService],
-    controllers: [HotelsController]
+    exports: [HotelsService],
+    controllers: [HotelsController],
+    providers: [HotelsService, PrismaService],
 })
