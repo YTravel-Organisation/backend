424d9c994bd823ef73fc4f179fbbf886ec6f002d
LSASSE <louis.sasse@protonmail.com>
2024-01-17 12:24:05 +0100

    feat: Add create hotel

--- apps/ytravel-backend/src/modules/hotels/hotels.service.ts
+++ apps/ytravel-backend/src/modules/hotels/hotels.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../../tools/database.config";
 
 
 @Injectable()
 export class HotelsService {
-    constructor() {}
+    constructor(
+        private prisma: PrismaService,
+    ) {}
+
+    async createHotel(createHotelDto) {
+        const hotel = await this.prisma.hotel.create({
+            data: {
+                ...createHotelDto,
+                createdAt: new Date(),
+            },
+        });
+        return hotel;
+    }
 }
