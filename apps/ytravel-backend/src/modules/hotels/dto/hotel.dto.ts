117428c4d3b30d485a41b1e659d1cad0f7a5cc93
LSASSE <louis.sasse@protonmail.com>
2024-01-17 09:57:14 +0100

    feat: Add check type in CreateHotelDto

--- apps/ytravel-backend/src/modules/hotels/dto/hotel.dto.ts
+++ apps/ytravel-backend/src/modules/hotels/dto/hotel.dto.ts
@@ -1,13 +1,41 @@
+import { IsLatitude, IsLongitude, IsNotEmpty, IsNumber, IsPhoneNumber } from "class-validator";
+
 export class CreateHotelDto {
+
+    @IsNotEmpty()
+    @IsNumber()
     admin_id: number;
+
+    @IsNotEmpty()
     name: string;
+
+    @IsNotEmpty()
     description: string;
+
+    @IsNotEmpty()
     address: string;
+
+    @IsNotEmpty()
     email: string;
+
+    @IsNotEmpty()
     website: string;
+
+    @IsNotEmpty()
+    @IsPhoneNumber()
     phone_number: string;
+
+    @IsNotEmpty()
+    @IsLongitude()
     longitude: number;
+
+    @IsNotEmpty()
+    @IsLatitude()
     latitude: number;
+
+    @IsNotEmpty()
     faq: string;
+
+    @IsNotEmpty()
     other_information: string;
 }
