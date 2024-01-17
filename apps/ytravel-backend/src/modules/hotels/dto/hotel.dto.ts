cabff54ce4ad8964bed59e0fbd9267732338367f
LSASSE <louis.sasse@protonmail.com>
2024-01-17 12:31:55 +0100

    feat: add controle param in hotel dto

--- apps/ytravel-backend/src/modules/hotels/dto/hotel.dto.ts
+++ apps/ytravel-backend/src/modules/hotels/dto/hotel.dto.ts
@@ -1,3 +1,5 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { AutoMap } from "nestjsx-automapper";
 import { 
     IsLatitude,
     IsLongitude, 
@@ -7,41 +9,62 @@ import {
 } from "class-validator";
 
 export class CreateHotelDto {
-
+    @ApiProperty()
+    @AutoMap()
     @IsNotEmpty()
     @IsNumber()
     admin_id: number;
 
+    @ApiProperty()
+    @AutoMap()
     @IsNotEmpty()
     name: string;
 
+    @ApiProperty()
+    @AutoMap()
     @IsNotEmpty()
     description: string;
 
+    @ApiProperty()
+    @AutoMap()
     @IsNotEmpty()
     address: string;
 
+    @ApiProperty()
+    @AutoMap()
     @IsNotEmpty()
     email: string;
 
+    @ApiProperty()
+    @AutoMap()
     @IsNotEmpty()
     website: string;
 
+    @ApiProperty()
+    @AutoMap()
     @IsNotEmpty()
     @IsPhoneNumber()
     phone_number: string;
 
+    @ApiProperty()
+    @AutoMap()
     @IsNotEmpty()
     @IsLongitude()
     longitude: number;
 
+    @ApiProperty()
+    @AutoMap()
     @IsNotEmpty()
     @IsLatitude()
     latitude: number;
 
+    @ApiProperty()
+    @AutoMap()
     @IsNotEmpty()
     faq: string;
 
+    @ApiProperty()
+    @AutoMap()
     @IsNotEmpty()
     other_information: string;
 }
