67521277debdae8f03d7ab03975e727f27d469be
MrBartou <53816747+MrBartou@users.noreply.github.com>
2024-04-30 15:59:04 +0200

    chore: linter correction

--- apps/ytraveling-backend/src/modules/notifications/dto/notification.dto.ts
+++ apps/ytraveling-backend/src/modules/notifications/dto/notification.dto.ts
@@ -4,59 +4,57 @@ import { ApiProperty } from '@nestjs/swagger';
 import { AutoMap } from 'nestjsx-automapper';
 
 export class CreateNotificationDto {
+  @ApiProperty()
+  @AutoMap()
+  @IsDate()
+  @IsOptional()
+  createdAt?: Date;
 
-    @ApiProperty()
-    @AutoMap()
-    @IsDate()
-    @IsOptional()
-    createdAt?: Date;
+  @ApiProperty()
+  @AutoMap()
+  @IsInt()
+  userId: number;
 
-    @ApiProperty()
-    @AutoMap()
-    @IsInt()
-    userId: number;
+  @ApiProperty()
+  @AutoMap()
+  @IsString()
+  type: NotificationType;
 
-    @ApiProperty()
-    @AutoMap()
-    @IsString()
-    type: NotificationType;
+  @ApiProperty()
+  @AutoMap()
+  @IsString()
+  title: string;
 
-    @ApiProperty()
-    @AutoMap()
-    @IsString()
-    title: string;
+  @ApiProperty()
+  @AutoMap()
+  @IsString()
+  message: string;
+}
 
-    @ApiProperty()
-    @AutoMap()
-    @IsString()
-    message: string;
-  }
+export class UpdateNotificationDto {
+  @ApiProperty()
+  @AutoMap()
+  @IsDate()
+  @IsOptional()
+  Datetime?: Date;
 
-  export class UpdateNotificationDto {
+  @ApiProperty()
+  @AutoMap()
+  @IsInt()
+  userId: number;
 
-    @ApiProperty()
-    @AutoMap()
-    @IsDate()
-    @IsOptional()
-    Datetime?: Date;
+  @ApiProperty()
+  @AutoMap()
+  @IsString()
+  type: NotificationType;
 
-    @ApiProperty()
-    @AutoMap()
-    @IsInt()
-    userId: number;
+  @ApiProperty()
+  @AutoMap()
+  @IsString()
+  title: string;
 
-    @ApiProperty()
-    @AutoMap()
-    @IsString()
-    type: NotificationType;
-
-    @ApiProperty()
-    @AutoMap()
-    @IsString()
-    title: string;
-
-    @ApiProperty()
-    @AutoMap()
-    @IsString()
-    message: string;
-  }
+  @ApiProperty()
+  @AutoMap()
+  @IsString()
+  message: string;
+}
